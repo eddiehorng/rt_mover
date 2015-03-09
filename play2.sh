d77f6cc62de47158d56e7319784af1dc83adb7ee
Eddie Horng (NB) <eddiehorng.tw@gmail.com>
2015-03-09 08:55:03 +0800

    update play2.sh

--- play2.sh
+++ play2.sh
@@ -13,12 +13,12 @@ check_match_period=6
 # ctrl_continue=( '200x48+98+1120' 'continue_crop.png' 355 905)
 # ctrl_network=( '580x110+80+685' 'dialog_crop.png' 510 735)
 
-declare -a c0=( '160x60+56+1130' 'endless_crop.png' 175 1160)
-declare -a c1=( '192x32+268+1120' 'attack_crop.png' 360 1140)
-declare -a c2=( '200x55+132+682' 'gameover_crop.png' 235 715)
-declare -a c3=( '186x48+264+888' 'gift_crop.png' 355 905)
-declare -a c4=( '200x48+98+1120' 'continue_crop.png' 195 1150)
-declare -a c5=( '580x110+80+685' 'dialog_crop.png' 510 735)
+declare -a c0=( '160x60+56+1130' 'endless_crop.png' 175 1160 0)
+declare -a c1=( '192x32+268+1120' 'attack_crop.png' 360 1140 0)
+declare -a c2=( '200x55+132+682' 'gameover_crop.png' 235 715 0)
+declare -a c3=( '186x48+264+888' 'gift_crop.png' 355 905 0)
+declare -a c4=( '200x48+98+1120' 'continue_crop.png' 195 1150 0)
+declare -a c5=( '580x110+80+685' 'dialog_crop.png' 510 735 0)
 c_num=6
 
 function printlog()
@@ -58,14 +58,20 @@ do
         x=${!var}
         var="c"$i"[3]"
         y=${!var}
-
+        last_occur_var="c"$i"[4]"        
+        last_occur=${!last_occur_var}
+        
         # echo ${!var}
 #         echo $location
 #         echo $pic
         convert screen.png -crop $location tmp.png
         s=`compare -metric MAE tmp.png $pic null: 2>&1`
-        if [ "$s" = "0 (0)" ]; then 
-            printlog "Found $pic"
+        if [ "$s" = "0 (0)" ]; then             
+            start=$last_occur
+            now=$(date +%s)
+            diff=`echo $((now-start)) | awk '{print int($1/60)":"int($1%60)}'`
+            printlog "Found $pic -- $diff"
+            let $last_occur_var=$now
             $adb_cmd $tap $x $y
             break
         fi
