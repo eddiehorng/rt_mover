ad6cf57c116300dede04bdcdaf16a758a8d59e79
Eddie Horng (NB) <eddiehorng.tw@gmail.com>
2015-05-09 07:02:38 +0800

    new attach1 crop

--- crop.sh
+++
@@ -1,1 +0,0 @@
-convert Screenshot_2015-03-01-11-19-04.png -crop 192x32+268+1120 attack_crop.png
--- play2.sh
+++ play2.sh
@@ -16,7 +16,8 @@ declare -a c4=( '200x48+98+1120' 'continue_crop.png' 195 1150 0)
 declare -a c5=( '580x110+80+685' 'dialog_crop.png' 510 735 0)
 declare -a c6=( '192x42+264+970' 'iknow_crop.png' 264 970 0)
 declare -a c7=( '37x27+342+964' 'fly_crop.png' 342+387 964+964 0)
-c_num=8
+declare -a c8=( '100x32+188+1120' 'attack1_crop.png' 190 1122 0)
+c_num=9
 
 function printlog()
 {
