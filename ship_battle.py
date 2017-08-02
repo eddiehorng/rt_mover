6f645772fae8626f8eeb2f7e1eb462e76bdc83f5
Eddie Horng <eddiehorng.tw@gmail.com>
2017-08-02 23:08:22 +0800

    upload

--- ship_battle.py
+++ ship_battle.py
@@ -4,7 +4,7 @@ from utils import *
 from ship_common import *
 
 do_enter_game=0
-num_match=14
+num_match=16
 
 if __name__ == '__main__':
     if do_enter_game:
@@ -14,24 +14,25 @@ if __name__ == '__main__':
         time.sleep(60)
     else:
         # back to main screen
-        click(1215, 72, 1)
-        click(1215, 72, 1)
+        click(1215, 72, 2)
+        click(1215, 72, 2)
+        click(1215, 72, 2)
 
     # drink
-    click(1110, 200, 2)
-    click(680, 640, 1)
-    click(1215, 72, 1)
+    click(1110, 200, 4)
+    click(680, 640, 2)
+    click(1215, 72,21)
 
     # enter battle
-    click(1233, 550, 3)
-    click(500, 550, 1)
+    click(1233, 550, 6)
+    click(500, 550, 3)
 
     # fight
     for _ in range(0, num_match):
         for _ in range(0, 3):
-            click(200, 670, 2)
-            click(500, 500, 1)
-        swipe(100, 600, 500, 500, 1)
+            click(200, 670, 3)
+            click(500, 500, 2)
+        swipe(100, 600, 500, 500, 2)
 
 
 
